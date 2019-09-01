9d47aabc1dbe9b09ec8fad7faf02db0c20168ca3
kango furusu <furusu0715@gmail.com>
2019-09-01 22:27:13 +0900

    nullの場合を考慮して nullの場合は{width: 0, height: 0}のような型を満たす値を返す

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ greet }}</h1>
-  </div>
+    <div class="hello">
+
+    </div>
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue';
+  import Vue, {PropType} from 'vue';
 
-export default Vue.extend({
-  name: 'HelloWorld',
-  computed: {
-    greet(): string {
-      return "Hello World!"
-    }
+  export type Rect = {
+    width: number
+    height: number
   }
-});
+  export type Position = {
+    top: number
+    left: number
+  }
+  export default Vue.extend({
+    name: 'HelloWorld',
+    data: () => ({
+      bounds: null as ClientRect | DOMRect | null
+    }),
+    methods: {
+      onClickElement({target}: { target: HTMLElement }): void {
+        this.bounds = target.getBoundingClientRect()
+      }
+    },
+    computed: {
+      boundInfo(): string {
+        if (this.bounds === null) return ""
+        return JSON.stringify(this.bounds)
+      },
+      rect(): Rect {
+        if (this.bounds === null) return {width: 0, height: 0}
+        return {
+          width: this.bounds.width,
+          height: this.bounds.height
+        }
+      },
+      position(): Position {
+        if (this.bounds === null) return {top: 0, left: 0}
+        return {
+          top: this.bounds.top,
+          left: this.bounds.left
+        }
+      }
+    }
+  });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
