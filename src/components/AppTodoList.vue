8cff1311423dd6e60e788873805c92ea45fe666a
kango furusu <furusu0715@gmail.com>
2019-09-01 17:12:17 +0900

    型推論と型アサーションで付与

---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script lang="ts">
+    import Vue from "vue"
+  export default Vue.extend({
+    name: "AppTodoList",
+    data: () => ({
+      firstName: '',
+      lastName: '',
+      doneTodos: null as null | boolean,
+      todos: [] as Array<{
+        id: string
+        task: string
+        done: boolean
+      }>
+    })
+  })
+</script>
+
+<style scoped>
+
+</style>
