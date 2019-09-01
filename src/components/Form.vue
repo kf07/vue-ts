85479faf4187239b1dd3cf2821fe2c7ae90051ce
kango furusu <furusu0715@gmail.com>
2019-09-01 22:13:53 +0900

    Date型を得るdateFormDateLabel関数 dateLabelがNullable型のためnullを配慮したコードをかく

---
+++ src/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+    import Vue from "vue"
+  export default Vue.extend({
+    name: "Form",
+    data:() => ({
+      defaultDateLavel: "1980-01-01",
+      dateLabel: null as string | null
+    }),
+    computed: {
+      dateFormLabel(): Date {
+        let label = this.defaultDateLavel
+        if (this.dateLabel !== null) {
+          label = this.dateLabel
+        }
+        return new Date(label)
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
